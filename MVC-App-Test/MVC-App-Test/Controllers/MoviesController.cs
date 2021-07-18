6da63b30e0248ff89987d222882ca4b809bd0cdb
Kras13 <paskalev.1989@abv.bg>
2021-07-18 22:01:15 +0300

    Update

---
+++ MVC-App-Test/MVC-App-Test/Controllers/MoviesController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MVC_App_Test.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MVC_App_Test.Controllers
+{
+    public class MoviesController : Controller
+    {
+        public IActionResult Random()
+        {
+            var movie = new Movie() { Name = "Shrek! " };
+
+            return View(movie);
+        }
+    }
+}
